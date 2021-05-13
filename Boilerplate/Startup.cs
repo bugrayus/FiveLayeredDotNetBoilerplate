1e3a07e2162ef5199d5ce95ce5e6e72e6aacf5b1
bugradurukan <bugray34@gmail.com>
2021-05-13 21:54:44 +0300

    last arrangement nuget

--- Boilerplate/Startup.cs
+++ Boilerplate/Startup.cs
@@ -34,7 +34,6 @@ namespace Boilerplate
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             var appSettingsSection = Configuration.GetSection("AppSettings");
@@ -139,7 +138,6 @@ namespace Boilerplate
             });
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
